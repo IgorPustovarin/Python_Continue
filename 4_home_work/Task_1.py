96bb225d302995f5a01d77ec2d035f453600f1cf
Igor P <adel-time@mail.ru>
2023-10-02 21:52:47 +0800

    Home_Work4

--- 4_home_work/Task_1.py
+++ 4_home_work/Task_1.py
@@ -1,14 +1,29 @@
 # Напишите функцию для транспонирования матрицы
+#
+# string_one = [1, 4, 6]
+# string_two = [3, 5, 9]
+# string_three = [2, 7, 0]
+# print(f'{string_one} \n{string_two} \n{string_three}\n')
+#
+# def transposition (string_one_, string_two_, string_three_):
+#     for i in range(len(string_one)):
+#         string_new1 = [string_one_[i], string_two_[i], string_three_[i]]
+#         print(string_new1)
+#     return string_new1
+# transposition(string_one, string_two, string_three) Это не правильно решение
 
-string_one = [1, 4, 6]
-string_two = [3, 5, 9]
-string_three = [2, 7, 0]
-print(f'{string_one} \n{string_two} \n{string_three}\n')
+# Это правильное решение (два варианта)
+my_matrix = [[1, 2, 3], [4, 5, 6]]
+def trans_for(matrix: list[list[int]]) -> list[list[int]]:
+    temp = []
+    for i in range(len(matrix[0])):
+        temp.append([])
+        for j in range(len(matrix)):
+            temp[i].append(matrix[j][i])
+        return temp
+def trans_zip(matrix: list[list[int]]) -> list[list[int]]:
+    return list(zip(*matrix))
 
-def transposition (string_one_, string_two_, string_three_):
-    for i in range(len(string_one)):
-        string_new1 = [string_one_[i], string_two_[i], string_three_[i]]
-        print(string_new1)
-    return string_new1
-transposition(string_one, string_two, string_three)
+print(trans_for(my_matrix))
+print(trans_zip(my_matrix))
 
--- 4_home_work/Task_2.py
+++ 4_home_work/Task_2.py
@@ -8,10 +8,44 @@
 #     my_resul = {my_list2_[i]: my_list_[i] for i in range(len(my_list))}
 #     return my_resul
 # print(magic(my_list, my_list2))
+#
+# my_list = {'myata': 1, 'slon': 2, 'dog': 3}
+#
+# def magic(my_list_):
+#     my_resul = {my_list.values(): my_list.items() for i in range(len(my_list))}
+#     return my_resul
+# print(magic(my_list)) Это не правильное решение
 
-my_list = {'myata': 1, 'slon': 2, 'dog': 3}
+# Это правильное решение (два варианта)
+my_dict = {1: 'one', 2: ['two'], '3': ('three',)}
 
-def magic(my_list_):
-    my_resul = {my_list.values(): my_list.items() for i in range(len(my_list))}
-    return my_resul
-print(magic(my_list))
+def inverse_try(**kwargs) -> dict:
+    new_dict = {}
+    for key, value in kwargs.items():
+        try:
+            new_dict[value] = key
+        except:
+            new_dict[str(value)] = key
+        return new_dict
+
+def inverse_instance(**kwargs) -> dict:
+    new_dict = {}
+    for key, value in kwargs.items():
+        if not isinstance(value, list | set | dict):
+            new_dict[value] = key
+        else:
+            new_dict[str(value)] = key
+        return new_dict
+
+def inverse_hash(**kwargs) -> dict:
+    new_dict = {}
+    for key, value in kwargs.items():
+        if value.__hashble__():
+            new_dict[value] = key
+        else:
+            new_dict[str(value)] = key
+        return new_dict
+
+print(inverse_try(one = 1, two=['2'], three=(3,)))
+print(inverse_instance(one = 1, two=['2'], three=(3,)))
+
